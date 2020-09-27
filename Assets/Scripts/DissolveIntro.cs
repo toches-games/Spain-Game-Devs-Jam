5fc854766aca6e4f0906e256ebfce7ff7d8adcd0
Jonathan Cantor <jonatcantor@gmail.com>
2020-09-27 14:03:09 -0400

    Added dissolve intro in negacion

---
+++ Assets/Scripts/DissolveIntro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DissolveIntro : MonoBehaviour
+{
+    public float dissolveDuration;
+
+    private Material material;
+
+    private float velocity;
+
+    private void Awake() {
+        material = GetComponent<Image>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.SetFloat("Fade", Mathf.SmoothDamp(material.GetFloat("Fade"), 1, ref velocity, dissolveDuration));   
+    }
+}
